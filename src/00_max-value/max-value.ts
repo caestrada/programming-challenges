d1d894b3751e18acd7d7b722840582dd03fdfba2
Carlos Estrada <cestr008@gmail.com>
2023-04-27 10:19:17 -0400

    Add first solution

--- src/00_max-value/max-value.ts
+++ src/00_max-value/max-value.ts
@@ -1,3 +1,10 @@
 export const maxValue = (nums: number[]): number => {
-  // TODO
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (max >= nums[i]) continue;
+
+    max = nums[i];
+  }
+
+  return max;
 };
