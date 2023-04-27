bf8a7cd6c20a74fd31c8ba94bd6ebc43a7221b48
Carlos Estrada <cestr008@gmail.com>
2023-04-27 10:14:15 -0400

    Add tests for first challenge

--- src/00_max-value/max-value.test.ts
+++ src/00_max-value/max-value.test.ts
@@ -1,7 +1,32 @@
 import { maxValue } from "./max-value";
 
-test("00_max-value", () => {
-  const input = [4, 7, 2, 8, 10, 9];
-  const output = 10;
-  expect(maxValue(input)).toBe(10);
+const tests = [
+  {
+    input: [4, 7, 2, 8, 10, 9],
+    output: 10,
+  },
+  {
+    input: [10, 5, 40, 40.3],
+    output: 40.3,
+  },
+  {
+    input: [-5, -2, -1, -11],
+    output: -1,
+  },
+  {
+    input: [42],
+    output: 42,
+  },
+  {
+    input: [1000, 8],
+    output: 1000,
+  },
+];
+
+describe("Max Value", () => {
+  tests.forEach((t, i) => {
+    test(`Test ${i}`, () => {
+      expect(maxValue(t.input)).toBe(t.output);
+    });
+  });
 });
