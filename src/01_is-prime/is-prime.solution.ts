ca898829f2b4aee82963ed432069c04956023e7c
Carlos Estrada <cestr008@gmail.com>
2023-05-19 08:56:03 -0400

    Add optimized solution

--- src/01_is-prime/is-prime.solution.ts
+++ src/01_is-prime/is-prime.solution.ts
@@ -1,7 +1,23 @@
+// Time:  O(n)
+// Space: O(1)
 export const isPrime = (n: number) => {
   if (n === 1) return false;
 
   for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// Time:  O(sqrt(n))
+// Space: O(1)
+export const isPrimeOptimized = (n: number) => {
+  if (n === 1) return false;
+
+  for (let i = 2; i < Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
     }
